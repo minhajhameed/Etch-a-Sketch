6994a84833dd668429494e130c7fa92396976d74
Minhaj Hameed <minhajham@yahoo.com>
2023-01-02 18:49:42 +0530

    added main content div

---
+++ main.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+
+const headContent = document.createElement("div");
+screen.classList.add("screen");
+container.appendChild(screen);
+
+const bottomContent = document.createElement("div");
+screen.classList.add("screen");
+container.appendChild(screen);
+
+const screen = document.createElement("div");
+screen.classList.add("screen");
+bottomContent.appendChild(screen);
+
+
